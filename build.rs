f0780d0450a4ea6e684d4218cfb7d41a2162ab71
Pasha Podolsky <ppodolsky@me.com>
2022-03-28 19:12:34 +0300

    - [summa] Update version   - [summa] Improve documentation   - [summa] Rework build.rs
    GitOrigin-RevId: da5376859da972a7e1d8e38c00329ab57f787893

--- build.rs
+++ build.rs
@@ -1,10 +1,13 @@
-use std::fs::{create_dir_all, remove_dir_all};
+use std::env;
+use std::fs::{canonicalize, create_dir_all, remove_dir_all};
 use std::os::unix::fs::symlink;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let proto_prefix = "summa";
-    create_dir_all(proto_prefix).unwrap();
-    symlink("../proto", proto_prefix.to_string() + "/proto").unwrap();
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let proto_prefix = format!("{}/{}", out_dir, "/summa");
+    let original_path = canonicalize("proto").unwrap();
+    create_dir_all(&proto_prefix).unwrap();
+    symlink(original_path, proto_prefix.to_string() + "/proto").unwrap();
     tonic_build::configure().compile(
         &[
             "summa/proto/consumer.proto",
@@ -13,7 +16,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             "summa/proto/index_service.proto",
             "summa/proto/search_service.proto",
         ],
-        &["./"],
+        &[&out_dir],
     )?;
     remove_dir_all(proto_prefix).unwrap();
     Ok(())
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![crate_name = "summa"]
-//! Fast full-text search server with following features:
+//! Fast full-text search server
 //!
+//! ## Features
 //! - Simple GRPC API for managing multiple indices
 //! - Indexing documents through Kafka or directly
 //! - IPFS API for replication (oncoming)
@@ -8,89 +9,22 @@
 //! - Various configurable tokenizers (including CJK)
 //! - Fine CLI on Python
 //!
-//! ## Quickstart
-//! #### Install
-//! ```bash
-//! cargo install summa
-//! pip install aiosumma
-//! ```
-//! #### Generate default config file
-//! ```bash
-//! mkdir data
-//! summa-server generate-config > data/summa.yaml
-//! ```
-//! #### Launch server
-//! ```bash
-//! summa-server serve data/summa.yaml
-//! ```
-//! #### Create index
-//! ```bash
-//! summa-client localhost:8082 create-index test-index --default-fields='["title", "body"]' "
-//! ---
-//! # yamllint disable rule:key-ordering
-//! - name: id
-//!   type: i64
-//!   options:
-//!     fast: single
-//!     fieldnorms: false
-//!     indexed: true
-//!     stored: true
-//! - name: body
-//!   type: text
-//!   options:
-//!     indexing:
-//!       fieldnorms: true
-//!       record: position
-//!       tokenizer: summa
-//!     stored: true
-//! - name: tags
-//!   type: text
-//!   options:
-//!     indexing:
-//!       fieldnorms: true
-//!       record: position
-//!       tokenizer: summa
-//!     stored: true
-//! - name: title
-//!   type: text
-//!   options:
-//!     indexing:
-//!       fieldnorms: true
-//!       record: position
-//!       tokenizer: summa
-//!     stored: true
-//! "
-//! ```
-//! #### Index documents
-//! ```bash
-//! summa-client localhost:8082 index-document test-index '{"id": 1, "title": "What We Know About Extraterrestrial Intelligence: Foundations of Xenology", \
-//! "body": "Have you ever wondered what could happen when we discover another communicating species outside the Earth? \
-//! This book addresses this question in all its complexity. In addition to the physical barriers for communication, \
-//! such as the enormous distances where a message can take centuries to reach its recipient, the book also examines \
-//! the biological problems of communicating between species, the problems of identifying a non-Terrestrial intelligence, \
-//! and the ethical, religious, legal and other problems of conducting discussions across light years. Most of the book is concerned \
-//! with issues that could impinge on your life: how do we share experiences with ETI? Can we make shared laws? Could we trade? \
-//! Would they have religion? The book addresses these and related issues, identifying potential barriers to communication and \
-//! suggesting ways we can overcome them. The book explores this topic through reference to human experience, through analogy and thought \
-//! experiment, while relying on what is known to-date about ourselves, our world, and the cosmos we live in.", "tags": ["science", "xenology"]}'
-//! ```
-//! #### Search
-//! ```bash
-//! summa-client localhost:8082 commit-index test-index
-//! summa-client localhost:8082 search test-index "xenology"
-//! ```
+//! ## Getting started
+//! - Look at [examples](crate::examples)
+//! - Explore [Docs.rs](https://docs.rs/crate/summa/latest)
 
 #[macro_use]
 extern crate lazy_static;
 
-mod apis;
+pub mod apis;
 pub mod configurator;
-mod consumers;
+pub mod consumers;
 pub mod errors;
-mod requests;
-mod search_engine;
+mod examples;
+pub mod requests;
+pub mod search_engine;
 pub mod servers;
-mod services;
+pub mod services;
 mod utils;
 
 pub mod proto {

2b85df288421a75a896d098215d1666407da77a1
pasha <ppodolsky@me.com>
2022-05-26 21:31:10 +0300

    [fix] Set proper conditions on workflow

--- aiosumma/aiosumma/eval_scorer_builder.py
+++ aiosumma/aiosumma/eval_scorer_builder.py
@@ -7,8 +7,9 @@ class EvalScorerBuilder:
     def __init__(self):
         self.ops = ['original_score']
 
-    def add_point_fade(self, field_name, point_of_time, interval: timedelta, baseline):
-        self.ops.append(f'point_fade({field_name}, {point_of_time}, {interval.total_seconds()}, {baseline})')
+    def add_gauss(self, field_name, point_of_time, offset: timedelta, interval: timedelta, decay):
+        self.ops.append(f'gauss({field_name}, {point_of_time}, {offset.total_seconds()}, '
+                        f'sigma_squared({interval.total_seconds()}, {decay}))')
 
     def add_fastsigm(self, field_name, alpha):
         self.ops.append(f'fastsigm({field_name}, {alpha})')

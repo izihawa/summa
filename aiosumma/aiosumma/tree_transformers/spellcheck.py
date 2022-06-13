e629c6e973da7ba8ead5f155f9b5c43f2bb0a5a0
pasha <ppodolsky@me.com>
2022-06-13 17:21:25 +0300

    [aiosumma] Do not remove incorrect variant while spell checking

--- aiosumma/aiosumma/tree_transformers/spellcheck.py
+++ aiosumma/aiosumma/tree_transformers/spellcheck.py
@@ -1,12 +1,16 @@
 from textblob import Word as TextBlobWord
-from ..parser.elements import Word
+from ..parser.elements import Group, Word
 from .base import TreeTransformer
 
 
 class SpellcheckTreeTransformer(TreeTransformer):
     def visit_group(self, node, context, parents=None):
         if context.language == 'en':
+            corrected_operands = []
             for operand in node.operands:
                 if isinstance(operand, Word):
-                    operand.value = str(TextBlobWord(operand.value).correct())
+                    corrected_value = str(TextBlobWord(operand.value).correct())
+                    if corrected_value != operand.value:
+                        operand = Group(Word(corrected_value), Word(operand.value))
+                corrected_operands.append(operand)
         return node, False

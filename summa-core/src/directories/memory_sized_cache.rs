e8996463b646210936ce37f0e1051d3a79adbdff
pasha <ppodolsky@me.com>
2023-01-27 13:02:58 +0100

    fix: Clearing out licensing

--- summa-core/src/directories/memory_sized_cache.rs
+++ summa-core/src/directories/memory_sized_cache.rs
@@ -1,22 +1,3 @@
-// Copyright (C) 2022 Quickwit, Inc.
-//
-// Quickwit is offered under the AGPL v3.0 and as commercial software.
-// For commercial licensing, contact us at hello@quickwit.io.
-//
-// AGPL:
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as
-// published by the Free Software Foundation, either version 3 of the
-// License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program. If not, see <http://www.gnu.org/licenses/>.
-
 use std::borrow::Borrow;
 use std::hash::Hash;
 use std::path::{Path, PathBuf};
@@ -64,7 +45,6 @@ impl<K: Hash + Eq> Drop for NeedMutMemorySizedCache<K> {
 }
 
 impl<K: Hash + Eq> NeedMutMemorySizedCache<K> {
-    /// Creates a new NeedMutSliceCache with the given capacity.
     fn with_capacity(capacity: Capacity, cache_metrics: CacheMetrics) -> Self {
         NeedMutMemorySizedCache {
             // The limit will be decided by the amount of memory in the cache,

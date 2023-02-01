7ef8e9648e5eeb502beb71711ae45e0d494c3901
pasha <ppodolsky@me.com>
2023-02-01 07:51:48 +0100

    feat: Refactor `summa-publisher`

--- summa-publisher/src/main.rs
+++ summa-publisher/src/main.rs
@@ -8,37 +8,15 @@ use iroh_rpc_types::store::StoreAddr;
 use iroh_unixfs::unixfs::UnixfsNode;
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize)]
-struct DataItem {
-    pub cid: String,
-    pub name: String,
-}
+static DEFAULT_CHUNK_SIZE: usize = 1024 * 1024;
+static DEFAULT_CODE: cid::multihash::Code = cid::multihash::Code::Blake3_256;
 
-#[derive(Deserialize, Serialize)]
-struct StoreConfig {
-    pub endpoint: String,
-}
-
-#[derive(Deserialize, Serialize)]
-struct LayoutConfig {
-    pub root: String,
-    pub data: Vec<DataItem>,
-}
-
-#[derive(Deserialize, Serialize)]
-struct Config {
-    store: StoreConfig,
-    layout: LayoutConfig,
-}
-
-static CHUNK_SIZE: usize = 1024 * 1024;
-
-async fn publish(store_endpoint: &str, root_path: &str, data_paths: &[String]) -> Result<Option<Cid>, Box<dyn Error>> {
-    let store_addr: StoreAddr = format!("irpc://{store_endpoint}").parse().expect("cannot parse");
-    let store_client = iroh_rpc_client::store::StoreClient::new(store_addr).await.expect("cannot create store client");
+async fn publish(store_endpoint: &str, root_path: &str, data_paths: &[String], chunk_size: usize) -> Result<Option<Cid>, Box<dyn Error>> {
+    let store_addr: StoreAddr = format!("irpc://{store_endpoint}").parse()?;
+    let store_client = iroh_rpc_client::store::StoreClient::new(store_addr).await?;
 
     let root_directory = iroh_unixfs::builder::DirectoryBuilder::new()
-        .chunker(iroh_unixfs::chunker::Chunker::Fixed(iroh_unixfs::chunker::Fixed { chunk_size: CHUNK_SIZE }))
+        .chunker(iroh_unixfs::chunker::Chunker::Fixed(iroh_unixfs::chunker::Fixed { chunk_size }))
         .add_path(root_path)
         .await?;
     let mut data_dir = iroh_unixfs::builder::DirectoryBuilder::new().name("data");
@@ -47,29 +25,28 @@ async fn publish(store_endpoint: &str, root_path: &str, data_paths: &[String]) -
         let items: Vec<_> = data_item.split(':').collect();
         let (name, cid) = (items[0], items[1]);
         let cid = Cid::from_str(cid)?;
-        let block = store_client.get(cid).await?.unwrap_or_else(|| panic!("Cannot find {cid} at store"));
+        let block = store_client.get(cid).await??;
         let node = UnixfsNode::decode(&cid, block)?;
         data_dir = data_dir.add_raw_block(iroh_unixfs::builder::RawBlock::new(name, node.encode(&cid::multihash::Code::Blake3_256)?));
     }
     let root_directory = root_directory.add_dir(data_dir.build()?)?.build()?;
 
-    let mut blocks = root_directory.encode(&cid::multihash::Code::Blake3_256);
+    let mut blocks = root_directory.encode(&DEFAULT_CODE);
     let mut chunk = Vec::new();
     let mut chunk_size = 0u64;
     let mut cid = None;
 
-    while let Some(block) = blocks.next().await {
-        let block = block.expect("cannot get block");
+    while let Some(block) = blocks.next().await? {
         let block_size = block.data().len() as u64 + block.links().len() as u64 * 128;
         cid = Some(*block.cid());
-        if chunk_size + block_size > CHUNK_SIZE as u64 {
-            store_client.put_many(std::mem::take(&mut chunk)).await.expect("cannot_put");
+        if chunk_size + block_size > chunk_size as u64 {
+            store_client.put_many(std::mem::take(&mut chunk)).await?;
             chunk_size = 0;
         }
         chunk.push(block.into_parts());
         chunk_size += block_size;
     }
-    store_client.put_many(chunk).await.expect("cannot_put");
+    store_client.put_many(chunk).await?;
     Ok(cid)
 }
 
@@ -84,26 +61,28 @@ async fn main() -> Result<(), Box<dyn Error>> {
         .arg(arg!(-v --verbose ... "Level of verbosity"))
         .subcommand(
             command!("publish")
-                .about("Publish data to storage")
+                .about("Tool composes CAR-file from a single directory and a list of CIDs that mounted under `/data` sub-directory")
                 .arg(arg!(-s <STORE_ENDPOINT> "Iroh Store endpoint").default_value("0.0.0.0:4402").num_args(1))
                 .arg(
                     arg!(-r <ROOT_PATH> "Path to files that will be put in the root of CAR")
                         .default_value("web/dist")
                         .num_args(1),
                 )
-                .arg(arg!(-d <DATA_PATH> "Names and cids of `data` directory members in format <name>:<cid>")),
+                .arg(arg!(-d <DATA_PATH> "Names and CIDs for mounting under `data` sub-directory in format `<name>:<cid>`"))
+                .arg(arg!(-c <CHUNK_SIZE> "Size of a single block published to IPFS").default_value(DEFAULT_CHUNK_SIZE).num_args(1)),
         )
         .get_matches();
 
     match matches.subcommand() {
         Some(("publish", submatches)) => {
-            let store_endpoint = submatches.get_one::<String>("STORE_ENDPOINT").expect("wrong STORE_ENDPOINT");
-            let root_path = submatches.get_one::<String>("ROOT_PATH").expect("wrong ROOT_PATH");
+            let store_endpoint = submatches.get_one::<String>("STORE_ENDPOINT")?;
+            let root_path = submatches.get_one::<String>("ROOT_PATH")?;
+            let chunk_size = submatches.get_one::<usize>("CHUNK_SIZE")?;
             let data_paths = submatches
                 .get_many::<String>("DATA_PATH")
                 .map(|x| x.cloned().collect::<Vec<String>>())
                 .unwrap_or_default();
-            let published_cid = publish(store_endpoint, root_path, &data_paths).await?.expect("no new cid").to_string();
+            let published_cid = publish(store_endpoint, root_path, &data_paths, *chunk_size).await??.to_string();
             println!("{published_cid}");
         }
         _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
